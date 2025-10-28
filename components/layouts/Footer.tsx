30a85940658e08121361b0050d7a16ee227e646f
Márcia B. Matias <marciab.matias@hotmail.com>
2025-10-28 19:28:16 +0000

    add credits to the logo designer

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,28 +1,34 @@
 "use client"
 import styles from './footer.module.css'
-import { Icon } from "@iconify/react";
-import { useTranslations } from 'next-intl';
+import {Icon} from "@iconify/react";
+import {useTranslations} from 'next-intl';
+
 export const Footer = () => {
     const t = useTranslations();
     return (
         <div className={styles.footer}>
             <div className={styles.footerSec}>
                 <h4>{t('footer.contacts')}</h4>
-                <a href="mailto:land.it.web.app@gmail.com" target="_blank">
-                    <Icon className={styles.contact} icon={"iconamoon:email"} height={"30px"}/>
-                </a>
+                <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", height: "60%" }}>
+                    <a href="mailto:land.it.web.app@gmail.com" target="_blank">
+                        <Icon className={styles.contact} icon={"iconamoon:email"} height={"30px"}/>
+                    </a>
+                    <span>{t('footer.design')} <a href="https://www.linkedin.com/in/mariaines-gomes" target="_blank">
+                        <span>Maria Inês Gomes</span>
+                    </a></span>
+                </div>
             </div>
             <div style={{gap: "10px"}} className={styles.footerSec}>
                 <h4>{t('footer.links')}</h4>
-                <a target="_blank" 
+                <a target="_blank"
                    href={"https://landit.pt"}>
                     {t('footer.LAND-IT')}</a>
 
-                <a target="_blank" 
+                <a target="_blank"
                    href={"https://drive.google.com/file/d/1VwBF_oKHChhNPTup_Fa1dikuQQEUek_x/view?usp=sharing"}>
                     {t('footer.podcast')}</a>
 
-                <a target="_blank" 
+                <a target="_blank"
                    href={"https://drive.google.com/drive/folders/1A9KU2nZ6xXibhZnzlcDErIp21YEacsZF?usp=sharing"}>
                     {t('footer.oigp')}</a>
 
