834ebe007588e3f77a1960af4b47093358eb9c00
Márcia B. Matias <marciab.matias@hotmail.com>
2025-11-26 12:34:50 +0000

    improve team roles

--- src/components/presentational/Team.tsx
+++ src/components/presentational/Team.tsx
@@ -1,14 +1,16 @@
 import "./team.css"
 import {User} from "lucide-react";
+import {translate} from "@docusaurus/Translate";
 
 type Person = { name: string, job: string };
 
 const people: Person[] = [
-    { name: "Eng. António Louro", job: '' },
-    { name: "Prof. João Moura Pires", job: '' },
-    { name: "Márcia Matias", job: 'Tech Lead' },
-    { name: "Francisco Freire", job: 'Full-Stack Engineer' },
-    { name: "Prof. Cristina Henriques", job: '' },
+    { name: "Eng. António Louro", job: translate({id:"team.member1.role"})},
+    { name: "Prof. João Moura Pires", job: translate({id:"team.member2.role"}) },
+    { name: "Márcia Matias", job: translate({id:"team.member3.role"}) },
+    { name: "Francisco Freire", job: translate({id:"team.member4.role"}) },
+    { name: "Francisca Silva", job: translate({id:"team.member5.role"}) },
+    { name: "Prof. Cristina Henriques", job: translate({id:"team.member6.role"}) },
 ];
 
 export const Team = () => {
